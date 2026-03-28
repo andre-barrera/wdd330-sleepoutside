5cbee66e7eea22b09e892218cdffa205207e03b2
John Carlo <ijcwasbornonthe15thofseptember@gmail.com>
2026-03-28 22:53:44 +0800

    w03: inidivual | not complete

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ build: {
   rollupOptions: {
     input: {
       main: resolve(__dirname, "src/index.html"),
+      listing: resolve(__dirname, "src/product_listing/index.html"),
       cart: resolve(__dirname, "src/cart/index.html"),
       checkout: resolve(__dirname, "src/checkout/index.html"),
       product: resolve(__dirname, "src/product_pages/index.html"),
